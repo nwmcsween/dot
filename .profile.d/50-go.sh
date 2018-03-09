3ed069b7303ef18cc0274751108c66a988be7a16
Nathan McSween <nm@dyn.so>
2018-03-09 17:30:21 +0000

    lang: add warning if dirs don't exist

--- .profile.d/50-go.sh
+++ .profile.d/50-go.sh
@@ -2,5 +2,7 @@ if type go > /dev/null 2>&1; then
   if [ -d ${GOPATH:-"$HOME/.go"} ]; then
     export GOPATH
     str_ipush 'PATH' "$gopath/bin" ':'
+  else
+    io_puts "GOPATH isn't a directory"
   fi
 fi
--- .profile.d/50-npm.sh
+++ .profile.d/50-npm.sh
@@ -1,5 +1,7 @@
 if type npm > /dev/null 2>&1; then
   if [ -d ${NPMPATH:-"$HOME/.npm/bin"} ]; then
     str_ipush 'PATH' "$NPMPATH" ':'
+  else
+    io_puts "NPMPATH isn't a directory"
   fi
 fi
