d973bbbd0c8e76b514673326a7b9680f42a2bbbf
nwm <nwm@helium.octo>
2019-03-16 19:56:47 +0000

    remove else clauses, no one wants to be spammed with messages

--- .profile.d/50-go.sh
+++ .profile.d/50-go.sh
@@ -2,7 +2,5 @@ if defined go; then
   if [ -d ${GOPATH:="$HOME/.go"} ]; then
     export GOPATH
     stripush 'PATH' "$GOPATH/bin" ':'
-  else
-    echo "GOPATH isn't a directory"
   fi
 fi
--- .profile.d/50-npm.sh
+++ .profile.d/50-npm.sh
@@ -1,7 +1,5 @@
 if defined npm; then
   if [ -d "${NPMPATH:="$HOME/.npm/bin"}" ]; then
     stripush 'PATH' "$NPMPATH" ':'
-  else
-    echo "NPMPATH isn't a directory"
   fi
 fi
