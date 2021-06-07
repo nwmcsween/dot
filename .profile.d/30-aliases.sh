2f387ea83521bd6b83f2bae7aa88cde9361c4214
Nathan McSween <nwm@ioc.tl>
2021-06-07 04:44:05 +0000

    Namespace projects

--- .profile.d/30-aliases.sh
+++ .profile.d/30-aliases.sh
@@ -1,5 +1,5 @@
 alias paste="curl -F file=@- https://0x0.st"
-alias gdot='/usr/bin/env git --git-dir=$HOME/pj/dot --work-tree=$HOME'
+alias gdot='/usr/bin/env git --git-dir=$HOME/pj/personal/dot --work-tree=$HOME'
 alias ls='ls --color=auto'
 alias l='ls --color=auto'
 alias ll='ls -l --color=auto'
